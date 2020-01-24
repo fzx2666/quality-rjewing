65678bf43bdd753af843a3864d9cb800af52cbab
rjewing <ewingryan98@gmail.com>
2020-01-24 14:21:57 -0500

    refactored code, added tests

--- roman.py
+++ roman.py
@@ -1,50 +1,24 @@
 
+def to_roman(num):
+    # we assert that num is and int and is in range because we cannot represent
+    # numbers below 1 or above 3999
+    assert isinstance(num, int)
+    assert 0 < num < 4000
 
+    arabic = [1, 4, 5, 9, 10, 40, 50, 90,
+              100, 400, 500, 900, 1000]
+    roman_chars = ["I", "IV", "V", "IX", "X", "XL",
+                   "L", "XC", "C", "CD", "D", "CM", "M"]
 
-class ToRoman():
+    i = len(arabic) - 1 # should always be 12
+    roman = ""
+    while num:
+        digit = num // arabic[i] # this gets us the most significant digit
+        num %= arabic[i] # this removes the most significant digit for the next pass
 
-    def __init__(self, num):
-        self.arabic = [1, 4, 5, 9, 10, 40, 50, 90,
-                       100, 400, 500, 900, 1000]
-        self.roman = ["I", "IV", "V", "IX", "X", "XL",
-                      "L", "XC", "C", "CD", "D", "CM", "M"]
+        # this appends the desired number of roman numerals to the string
+        roman += (roman_chars[i] * digit)
 
-        self.arabic_num = num
-        self.roman_num = self.to_roman()
-        self.__call__()
+        i -= 1
 
-    def __call__(self):
-        print(f"Arabic: {self.arabic_num}")
-        print(f"Roman: {self.roman_num}")
-
-
-    def to_roman(self):
-        # we assert that the number is in range because we cannot represent
-        # numbers below 1 or above 3999
-        assert self.arabic_num > 0
-        assert self.arabic_num < 4000
-        tmp = self.arabic_num
-        i = len(self.arabic) - 1 # should always be 12
-        roman = ""
-        while tmp:
-            digit = tmp // self.arabic[i] # this gets us the most significant digit
-            tmp %= self.arabic[i] # this removes the most significant digit for the next pass
-
-            # this appends the desired number of roman numerals to the string
-            roman += (self.roman[i] * digit)
-
-            i -= 1
-
-        return roman
-
-
-
-
-if __name__ == '__main__':
-    ToRoman(123)
-    ToRoman(1)
-    ToRoman(3)
-    ToRoman(4)
-    ToRoman(3999)
-    ToRoman(0)
-    ToRoman(5000)
+    return roman
---
+++ tests/test_roman.py
@@ -0,0 +1,30 @@
+import os, sys
+print(__file__)
+currentdir = os.path.dirname(__file__)
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import pytest
+from roman import to_roman
+
+def test_roman_in_range():
+    assert to_roman(1) == 'I'
+    assert to_roman(3999) == 'MMMCMXCIX'
+    assert to_roman(123) == 'CXXIII'
+    assert to_roman(4) == 'IV'
+
+def test_roman_out_of_range():
+    with pytest.raises(AssertionError):
+        to_roman(0)
+    with pytest.raises(AssertionError):
+        to_roman(4000)
+    with pytest.raises(AssertionError):
+        to_roman(-100)
+
+def test_roman_wrong_input():
+    with pytest.raises(AssertionError):
+        to_roman('123')
+    with pytest.raises(AssertionError):
+        to_roman(5.0)
+    with pytest.raises(AssertionError):
+        to_roman('ABCDEF')
